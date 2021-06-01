b10dbcc688720dde9d5fd42393b8265016625712
ebochat <73542123+rbardinet@users.noreply.github.com>
2021-06-01 17:44:11 +0200

    API Chatbot communication interface
    Communication directe et ajout de messages sans avoir besoin de relancer la page, l'API chat bot est solicité via un fetch url et la réponse de l'API est traitée par le JS de la page web

--- API_Chatbot/routes/interface_com.js
+++ API_Chatbot/routes/interface_com.js
@@ -19,13 +19,38 @@ router.post('/:nom', function (req, res, next) {
     newchatbot = gest.addNewChatBot(req.body.name, req.body.login);
 
     console.log("ajout d'un nouveaux chatbot")
-    console.log(req.params);
-    console.log(JSON.stringify(req.body));
+    //console.log(req.params);
+    console.log(req.body);
 
 
     //Si il s'agit du première accès à la page de tchat, le bot ne dit rien
-    if(req.body.status=='firstAccess'){
-        res.render('chat_box.ejs', { chatbot_name: req.params.nom , userLogin: req.body.login});
+    if(req.body.isFirstAccess=='yes'){
+        res.render('chat_box.ejs', { chatbot_name: req.params.nom , userLogin: req.body.login, botReply: undefined});
+    }
+
+    //Si au contraire un message est envoyé par l'utilisateur, on le traite et on renvoie une réponse
+    if (req.body.isFirstAccess == 'no') {
+        console.log("generating bot reply")
+
+        try {
+            
+            let gest = Gestionnaire.getInstance();
+            chatbot = gest.addNewChatBot(req.body.name, req.body.userChatting);
+            console.log(gest.getChatBotInfos(req.body.name));
+            console.log(chatbot.getInfos());
+
+            let botReply = undefined;
+            gest.getChatBotByName(req.body.name).bot.sortReplies();
+            gest.getChatBotByName(req.body.name).getReply(req.body.userChatting, req.body.userMessage).then((reply) => console.log("generated reply :" + reply));
+            //console.log("generated chatbot reply :" + botReply);
+
+            //res.render('chat_box.ejs', { chatbot_name: req.params.nom, userLogin: req.body.login, botReply: "dummy reply" });
+            res.send(JSON.stringify({ 'chatbot_name': req.params.nom, 'userLogin': req.body.login, 'botReply': "dummy reply" }));
+            
+        } catch (err) {
+            console.error(err);
+            res.send(JSON.stringify({ 'chatbot_name': req.params.nom, 'userLogin': req.body.login, 'botReply': err }));
+        }
     }
 
 });
